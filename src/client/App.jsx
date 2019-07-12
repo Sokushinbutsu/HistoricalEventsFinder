70c1cca453150865d5cae61f1e3ea6e31c83f418
Brian Nelson <brian.allen.nilsson@gmail.com>
2019-07-12 14:32:48 -0400

    boilerplate

---
+++ src/client/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return <p>React!</p>;
+  }
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const webpack = require('webpack');
+const path = require('path');
+const LodashModuleReplacementPlugin = require('lodash-webpack-plugin');
+
+const config = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        use: 'babel-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  plugins: [new LodashModuleReplacementPlugin()]
+};
+
+module.exports = config;
