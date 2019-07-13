82188d01f75b3d2c4880c50fa6bb64eeaf7cc0df
Brian Nelson <brian.allen.nilsson@gmail.com>
2019-07-13 16:56:44 -0400

    Entry is now stateless

--- src/client/components/Entry.jsx
+++ src/client/components/Entry.jsx
@@ -1,22 +1,37 @@
 import React, { Component } from 'react';
 
-class Entry extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-  render() {
-    let results = this.props.data.map(function(item, index) {
-      return (
-        <div key={index}>
-          <b>{new Date(item.date).toString()}</b>
-          <p>{item.description}</p>
-        </div>
-      );
-    });
+// class Entry extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {};
+//   }
+//   render() {
+//     let results = this.props.data.map(function(item, index) {
+//       return (
+//         <div key={index}>
+//           <b>{new Date(item.date).toString()}</b>
+//           <p>{item.description}</p>
+//         </div>
+//       );
+//     });
 
-    return <ul>{results}</ul>;
-  }
-}
+//     return <ul>{results}</ul>;
+//   }
+// }
+
+const Entry = props => {
+  let results = props.data.map(function(item, index) {
+    return (
+      <div key={index}>
+        <b>{new Date(item.date).toString()}</b>
+        <p>{item.description}</p>
+      </div>
+    );
+  });
+
+  return <ul>{results}</ul>;
+};
 
 export default Entry;
+
+// export default Entry;
